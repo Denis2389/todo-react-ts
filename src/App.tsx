ccee0d5702251af3027004f2e6b029c3be8a7021
Denis2389 <bereznyakdenis2003@gmail.com>
2024-10-13 15:36:50 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,14 @@
-import React, { FC, ChangeEvent ,useState } from "react"
- 
-const App: FC = () => {
+import ListGroup from "./components/ListGroup"
 
-  const [task, setTask] = useState<string>("")
-  const [deadlint, setDeadline] = useState<number>(0)
-  const [todo, setTodoList] = useState([])
+const App = () => {
+  const items = ["New York", "San Francisko", "Tokyo", "London", "Paris"];
 
-  const handleChange = (event :ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.name === 'task') {
-      setTask(event.target.value)
-    } else {
-      setDeadline(Number(event.target.value))
-    }
-  }
-
-  const addTask = (): void => {
-    
-  }
 
   return (
-    <div className="App">
-      <div className="header">
-        <div className="inputContainer">
-        <input type='text' placeholder="Task..." name='task' onChange={handleChange}/>
-        <input type='number' placeholder="Deadlint (in Days)..." name="deadline" onChange={handleChange}/>
-        </div>
-        <button>Add Task</button>
-      </div>
-      <div className="todolist"></div>
+    <div>
+      <ListGroup items={items} heading="Cities"/>
     </div>
-  )
+  );
 }
-
-const Todolist = () => {
-
-}
-
 
 export default App
